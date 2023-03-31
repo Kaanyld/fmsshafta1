6ecdc0371ef5dccf08c79834b8f8580b1443b255
Kaan <kaanyldrm98@gmail.com>
2023-04-01 02:57:41 +0300

    commit

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-import axios from "axios"
-const getData = async (id) => { 
+import axios from "axios" // axios kütüphanesi içe aktarıldı
+const getData = async (id) => {     //async fonksiyon tanımlandı
 
-    const {data:users} = await axios (`https://jsonplaceholder.typicode.com/users/${id}`)
-    const {data:posts} = await axios (`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+    const {data:users} = await axios (`https://jsonplaceholder.typicode.com/users/${id}`) //Kullanıcı verilerini almak için istek gönderildi
+    const {data:posts} = await axios (`https://jsonplaceholder.typicode.com/posts?userId=${id}`) // Gönderinin verilerini almak için istek gönderildi
     
     
 console.log("User:",users)
 console.log("Post:",posts)    
-
+//Kullancı ve gönderileri konsola yazdırıldı.
 }
 export default getData;
